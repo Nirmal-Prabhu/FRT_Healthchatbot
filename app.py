25c3da6c9f81a390ae4d66e7e09129398220e58a
Nirmal Prabhu <33906725+Nirmal-Prabhu@users.noreply.github.com>
2022-06-25 22:06:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,6 @@
 # Importing essential libraries
 from flask import Flask, render_template, request
-import pickle
-import numpy as np
 
-# Load the Random Forest CLassifier model
-filename = 'diabetes-prediction-rfc-model.pkl'
-classifier = pickle.load(open(filename, 'rb'))
 
 app = Flask(__name__)
 
